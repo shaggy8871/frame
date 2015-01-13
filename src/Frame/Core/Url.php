7752b423e2e7112772bc1523034b9f380ae928a6
John Ginsberg <john.ginsberg@gmail.com>
2015-01-13 16:46:41 +0000

    Fixing duplicate query string

--- src/Frame/Core/Url.php
+++ src/Frame/Core/Url.php
@@ -33,6 +33,9 @@ class Url
             (isset($_SERVER['FRAME_REQUEST_URI']) ? $_SERVER['FRAME_REQUEST_URI'] :
             (isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : $_SERVER['REQUEST_URI']
         ));
+        if (strpos($requestUri, '?') !== false) {
+            $requestUri = strstr($requestUri, '?', true);
+        }
 
         $pathComponents = parse_url($this->scheme . '://' . $this->host . $requestUri . (isset($_SERVER['QUERY_STRING']) ? '?' . $_SERVER['QUERY_STRING'] : ''));
 

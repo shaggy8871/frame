c9fe330b26122f9a501f863844822ed9b67070f3
John Ginsberg <john.ginsberg@gmail.com>
2015-01-20 15:24:40 +0000

    Jsonp response support

---
+++ src/Frame/Response/Jsonp.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Frame\Response;
+
+class Jsonp extends Foundation implements ResponseInterface
+{
+
+    protected $contentType = 'application/json';
+    protected $callback = 'callback';
+
+    /*
+     * Render content in Jsonp encoded format
+     */
+    public function render($params = null)
+    {
+
+        $params = ($params ?: $this->viewParams);
+
+        if (!headers_sent()) {
+            http_response_code($this->statusCode);
+            header('Content-Type: ' . $this->contentType);
+        }
+
+        echo sprintf('%s(%s)', $this->callback, json_encode($params));
+
+    }
+
+    /*
+     * Set another callback function name if not the default "callback"
+     */
+    public function setCallback($callback)
+    {
+
+        $this->callback = $callback;
+
+    }
+
+}

65d42dbcd7315f7adc7cf1053d6ea1a590632a17
John Ginsberg <john.ginsberg@gmail.com>
2015-02-09 16:12:22 +0000

    Fixing controller reference

--- src/Frame/Core/Router.php
+++ src/Frame/Core/Router.php
@@ -283,7 +283,7 @@ class Router
                 $paramClass = $param->getClass();
             } catch (\Exception $e) {
                 // Rethrow the error with further information
-                throw new ClassNotFoundException($param->getName(), $this->caller->controller, $this->caller->method);
+                throw new ClassNotFoundException($param->getName(), ($this->caller->controller ? $this->caller->controller->getName() : null), $this->caller->method);
             }
             // If it's not a class, inject a null value
             if (!($paramClass instanceof \ReflectionClass)) {
